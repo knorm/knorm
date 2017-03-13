4dd9490d74d614d2d8def6a82e16bec7e32e2dec
Joel Mukuthu <jmu@one.com>
2017-03-13 18:08:02 +0100

    Fix createError helper tests

--- test/createErrors.spec.js
+++ test/createErrors.spec.js
@@ -1,4 +1,4 @@
-const createErrors = require('../../../lib/helpers/createErrors');
+const createErrors = require('../lib/createError');
 const createError = require('createerror');
 const httpErrors = require('httperrors');
 
@@ -10,19 +10,19 @@ describe('createErrors helper', function () {
     describe('when passed a string as the error name', function () {
         it('creates an error that inherits from the error defined by the second parameter', function () {
             const ParentError = createError({
-                name: 'BarError'
+                name: 'BarError',
             });
             const CreatedError = createErrors('FooError', ParentError);
             return expect(new CreatedError(), 'to satisfy', {
                 FooError: true,
-                BarError: true
+                BarError: true,
             });
         });
 
         it('creates an error without a parent if not passed a second parameter', function () {
             const CreatedError = createErrors('FooError');
             return expect(new CreatedError(), 'to satisfy', {
-                FooError: true
+                FooError: true,
             });
         });
     });
@@ -31,48 +31,48 @@ describe('createErrors helper', function () {
         it('creates an error named ErrorName that inherits from ParentError', function () {
             const parent = createError();
             return expect(createErrors({
-                FooError: parent
+                FooError: parent,
             }), 'to satisfy', {
                 FooError: createError({
-                    name: 'FooError'
-                }, parent)
+                    name: 'FooError',
+                }, parent),
             });
         });
 
         it('creates an error that does not inherit from anything if ParentError is null', function () {
             return expect(createErrors({
-                FooError: null
+                FooError: null,
             }), 'to satisfy', {
                 FooError: createError({
-                    name: 'FooError'
-                })
+                    name: 'FooError',
+                }),
             });
         });
 
         it('creates an error that inherits from httpErrors.BadRequest if ParentError is provided as "BadRequest"', function () {
             return expect(createErrors({
-                FooError: 'BadRequest'
+                FooError: 'BadRequest',
             }), 'to satisfy', {
                 FooError: createError({
-                    name: 'FooError'
-                }, httpErrors.BadRequest)
+                    name: 'FooError',
+                }, httpErrors.BadRequest),
             });
         });
 
         it('creates an error that inherits from httpErrors.BadRequest if ParentError is provided as "400"', function () {
             return expect(createErrors({
-                FooError: 400
+                FooError: 400,
             }), 'to satisfy', {
                 FooError: createError({
-                    name: 'FooError'
-                }, httpErrors.BadRequest)
+                    name: 'FooError',
+                }, httpErrors.BadRequest),
             });
         });
 
         it('throws an error if ParentError is provided as a string but is not one of the httpErrors', function () {
             return expect(function () {
                 createErrors({
-                    FooError: 'blah'
+                    FooError: 'blah',
                 });
             }, 'to throw', /Invalid parent error/);
         });
@@ -80,7 +80,7 @@ describe('createErrors helper', function () {
         it('throws an error if ParentError is provided as a number but is not one of the httpErrors', function () {
             return expect(function () {
                 createErrors({
-                    FooError: 10000
+                    FooError: 10000,
                 });
             }, 'to throw', /Invalid parent error/);
         });
@@ -88,14 +88,14 @@ describe('createErrors helper', function () {
         it('creates an error for every key in the object', function () {
             return expect(createErrors({
                 FooError: 400,
-                BarError: 503
+                BarError: 503,
             }), 'to satisfy', {
                 FooError: createError({
-                    name: 'FooError'
+                    name: 'FooError',
                 }, httpErrors.BadRequest),
                 BarError: createError({
-                    name: 'BarError'
-                }, httpErrors.TemporarilyUnavailable)
+                    name: 'BarError',
+                }, httpErrors.TemporarilyUnavailable),
             });
         });
     });
