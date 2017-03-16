4aa5389cf9e97853141faf87c184c1af496c3dc8
Joel Mukuthu <jmu@one.com>
2017-03-16 10:09:01 +0100

    Fix postgres connection on CI

--- test/lib/knex.js
+++ test/lib/knex.js
@@ -6,9 +6,10 @@ module.exports = () => {
         client = knex({
             client: 'pg',
             connection: {
-                host: '127.0.0.1',
-                user: 'postgres',
-                database: 'postgres',
+                host: process.env.POSTGRES_HOST || '127.0.0.1',
+                user: process.env.POSTGRES_USER || 'postgres',
+                password: process.env.POSTGRES_PASSWORD || '',
+                database: process.env.POSTGRES_DB || 'postgres',
             },
         });
     }
