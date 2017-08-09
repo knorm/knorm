06941683841c6a017ec932cfb568ec0c0814076f
Joel Mukuthu <jmukuthu@ganintegrity.com>
2017-08-09 18:37:18 +0200

    fix: avoid circular dependency issues

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const modelWithSoftDelete = require('./lib/modelWithSoftDelete');
-const queryWithSoftDelete = require('./lib/queryWithSoftDelete');
-const { Model: KnormModel, Query: KnormQuery } = require('knorm');
-
 module.exports = (Base, config) => {
+  const modelWithSoftDelete = require('./lib/modelWithSoftDelete');
+  const queryWithSoftDelete = require('./lib/queryWithSoftDelete');
+  const { Model: KnormModel, Query: KnormQuery } = require('knorm');
+
   if (Base.prototype instanceof KnormModel || Base === KnormModel) {
     return modelWithSoftDelete(Base, config);
   }
