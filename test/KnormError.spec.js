c785150cd6f0458f2f52e12a1fb06c798a719746
Joel Mukuthu <joelmukuthu@gmail.com>
2017-12-20 22:46:30 +0100

    fix(KnormError): support error messages

--- test/KnormError.spec.js
+++ test/KnormError.spec.js
@@ -11,6 +11,10 @@ describe('KnormError', () => {
       expect(new KnormError().name, 'to be', 'KnormError');
     });
 
+    it('supports passing a message like regular Errors', () => {
+      expect(new KnormError('foo').message, 'to be', 'foo');
+    });
+
     it('captures the stack trace', () => {
       expect(new KnormError().stack, 'to be a string');
     });
