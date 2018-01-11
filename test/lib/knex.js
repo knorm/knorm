682ce02437c9e1253f3f42fda6fe26b379a7e9d2
Joel Mukuthu <joelmukuthu@gmail.com>
2018-01-11 23:26:23 +0100

    chore: swap knorm-postgres with docker-compose

--- test/lib/knex.js
+++ test/lib/knex.js
@@ -1,9 +1,12 @@
 const knex = require('knex');
-const { config } = require('knorm-postgres');
 
-const client = knex({
+module.exports = knex({
   client: 'pg',
-  connection: config
+  connection: {
+    host: '127.0.0.1',
+    port: process.env.POSTGRES_PORT || 5432,
+    user: process.env.POSTGRES_USER || 'postgres',
+    password: process.env.POSTGRES_PASSWORD || '',
+    database: process.env.POSTGRES_DB || 'postgres'
+  }
 });
-
-module.exports = client;
