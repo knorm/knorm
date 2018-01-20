8ce910afdf5e6d62dc06cf23497af35f6e0191b2
Joel Mukuthu <joelmukuthu@gmail.com>
2018-01-20 21:30:53 +0100

    chore(package): fix postgres script

--- test/lib/knex.js
+++ test/lib/knex.js
@@ -3,7 +3,7 @@ const knex = require('knex');
 module.exports = knex({
   client: 'pg',
   connection: {
-    host: '127.0.0.1',
+    host: process.env.POSTGRES_HOST || '127.0.0.1',
     port: process.env.POSTGRES_PORT || 5432,
     user: process.env.POSTGRES_USER || 'postgres',
     password: process.env.POSTGRES_PASSWORD || '',
