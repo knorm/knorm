67a162eb07a7cc6a906219ef515663caaae870c8
Joel Mukuthu <joelmukuthu@gmail.com>
2018-03-25 19:38:53 +0200

    fix(orm): add fieldToColumn config

--- orm.js
+++ orm.js
@@ -2,8 +2,9 @@ const knexConfig = require('./knexfile');
 const makeKnex = require('knex');
 const makeKnorm = require('knorm');
 const knormTimestamps = require('knorm-timestamps');
+const { snakeCase: fieldToColumm } = require('lodash');
 
 const knex = makeKnex(knexConfig);
-const orm = makeKnorm({ knex }).use(knormTimestamps());
+const orm = makeKnorm({ knex, fieldToColumm }).use(knormTimestamps());
 
 module.exports = orm;
