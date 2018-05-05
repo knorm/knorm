3ce200fdadb49f5acb28a40bc9bb87f1d33d9b56
Joel Mukuthu <joelmukuthu@gmail.com>
2018-05-05 22:47:58 +0200

    tests(Query): fix test for `updated: false` fields

--- test/Query.spec.js
+++ test/Query.spec.js
@@ -1932,8 +1932,7 @@ describe('Query', () => {
             expect.it(
               'when passed as parameter to',
               query => query.toString(),
-              'not to contain',
-              'SET id ='
+              expect.it('to contain', 'name =').and('not to contain', ' id =')
             )
           );
         });
