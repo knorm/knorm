cbd90857f79de4eecccf1068a46058793f79905f
Joel Mukuthu <joelmukuthu@gmail.com>
2019-02-02 23:15:57 +0100

    refactor: minor

--- orm.js
+++ orm.js
@@ -3,9 +3,8 @@ const knormTimestamps = require('@knorm/timestamps');
 const knormPostgres = require('@knorm/postgres');
 const knormRelations = require('@knorm/relations');
 
-const host = process.env.PGHOST || '127.0.0.1';
 const connection = {
-  host,
+  host: process.env.PGHOST || '127.0.0.1',
   port: process.env.PGPORT || 5432,
   user: process.env.PGUSER || 'postgres',
   password: process.env.PGPASSWORD || '',
