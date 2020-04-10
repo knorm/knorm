5a7bba8336e38dfe76ac0ebfc29219cc0e0ed97b
Joel Mukuthu <joelmukuthu@gmail.com>
2020-04-10 14:21:09 +0200

    chore: remove yet-to-be-published package

--- packages/field-name-to-column-name/src/index.js
+++
@@ -1,6 +0,0 @@
-const KnormVirtuals = require('./KnormVirtuals');
-const knormVirtuals = config => new KnormVirtuals(config);
-
-knormVirtuals.KnormVirtuals = KnormVirtuals;
-
-module.exports = knormVirtuals;
